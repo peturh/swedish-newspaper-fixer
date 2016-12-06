01edc5b9774b5044d7a69af4aef6cf0120a6f5f1
petur <petur.hjartarson@ecsolutions.se>
2016-12-06 09:39:21 +0100

    removed jquery dependency. removed timeout.

--- fixer.js
+++ fixer.js
@@ -1,18 +1,8 @@
-//Run the method
-init();
-function init() {
-    //When window loads
-    window.onload = function () {
-        //Set timeout, since paywall is not appearing until after 1.5 seconds aprox.
-        setTimeout(function(){
-            //This is the brödtext
-            $(".article__body--mask .article__lead").css("max-height", "1000px");
-            //This is the klarna one press button buy box
-            $(".paywall--active").css("display","none");
-            //This removes small shadow on brödtext
-            $('.article__body-grid-item > p').css("margin-bottom","2.125rem");
-            //This is the article
-            $(".article__body--mask .article__premium-content").css("display","block");
-        },2300);
-    };
+function removePaywall() {
+    var paywall = document.getElementsByClassName('js-paywall');
+    for (var index = 0; index < paywall.length; index++) {
+        var element = paywall[index];
+        element.parentNode.removeChild(element);
+    }
 }
+removePaywall();
