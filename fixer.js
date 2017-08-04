94f55b178cfd192459e5eaa398e1430806230ca3
Pétur Hjartarson <petur.hjartarson@bimobject.com>
2017-08-04 08:18:42 +0200

    removed old stuff

--- fixer.js
+++ fixer.js
@@ -13,25 +13,13 @@ if(window.location.href.indexOf('dn')>1){
 if(window.location.href.indexOf('di')>1){
 //When dom is ready, remove ad header
     $(document).ready(function(){
-        console.log('yo')
-s
         var body = $('body');
         // Append menu button to navbar instead
         var menu = body.find('.di_header__menu').css('margin', '5px 0 0 15px');
         var navbar = body.find('.di_top-nav__main');
-
         navbar.prepend(menu);
-
         // Remove big fat header which makes the page useless on smaller screens
-
         body.find('.di_header').remove();
-
-        //Remove for both small screen and big screen view
-        body.find('.di_panorama-compensation').css('height',0);
-        body.find('.di_panorama.js_panorama').css('height',0);
-        body.find('.di_panorama-wrapper').css('height',0);
-        //Untested, but fixed the small, small bar at the top on desktop
-        body.find('.di_panorama').css('top','-6px');
     });
 
 //Run the init() method
